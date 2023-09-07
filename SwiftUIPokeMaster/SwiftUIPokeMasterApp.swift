6453669bd2b2d77ddfc44d7e55d860663f554248
c4ibD3 <280206014@qq.com>
2023-09-07 19:13:21 +0800

    Initial Commit

---
+++ SwiftUIPokeMaster/SwiftUIPokeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPokeMasterApp.swift
+//  SwiftUIPokeMaster
+//
+//  Created by 张鹏 on 2023/9/7.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPokeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
